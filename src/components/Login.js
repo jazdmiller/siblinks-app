15f9a1bae14215945edeb412c094e24fa4a5dbf7
Jazmine Miller <jazminedmiller@gmail.com>
2023-09-07 15:30:02 -0400

    add header logo style

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Login() {
   return (
     <div className='login-page'>
-        <header className='row text-center'>
-            <div className='my-4 header-logo'><span>Sib</span>Links</div>
+        <header className='row text-center w-100'>
+            <div className='my-4 header-logo'><span style={{fontFamily: "Nyght Serif Italic"}}>Sib</span>Links</div>
         </header>
     </div>
   )
