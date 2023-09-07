b704f555adb12ad71e429d3cac1c94e025beb232
Jazmine Miller <jazminedmiller@gmail.com>
2023-09-07 14:35:47 -0400

    add gradient css (login page)

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Login() {
   return (
     <div className='login-page'>
-        
+        <header>
+            <span>Sib</span>Links
+        </header>
     </div>
   )
 }
