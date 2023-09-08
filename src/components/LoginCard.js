60968606c9197e7ee914f21b773ad409a6a8481c
Jazmine Miller <jazminedmiller@gmail.com>
2023-09-08 09:33:29 -0400

    add form to login component

--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -2,21 +2,45 @@ import React from 'react'
 
 function LoginCard() {
   return (
-        <div className='card w-50'>
+        <div className='card login-card'>
             <div className='card-header'>
                 <div className='row justify-content-between'>
                     <div className='col'>Log In</div>
                     <div className='col text-end'>Sign Up</div>
                 </div>
             </div>
-            <div className='card-body mt-4 p-5'>
-                <ul className='list-group list-group-flush'>
+            <div className='card-body mt-4 p-md-5'>
+                <form>
+
+                    <div className=' form-label'>
+                    <label>
+                       Email address 
+                    </label>
+                    <div>
+                    <input className='w-100' type="text" name="email address" />
+                    </div>
+                    </div>
+
+                    <div className=' form-label'>
+                    <label>
+                        Password
+                    </label>
+                    <div>
+                        <input className="w-100" type="text" name="password"/>
+                    </div>
+                    </div>
+
+                    <div className='text-center form-label'>
+                        <input type="submit" value="Log In" />
+                    </div>
+                {/* <ul className='list-group list-group-flush'>
                     <li className='list-group-item p-3'>Email address</li>
                     <li className='list-group-item p-3'>Password</li>
                     <li className='list-group-item p-3'>
                         <button>Log In</button>
                     </li>
-                </ul>
+                </ul> */}
+                </form>
             </div>
         </div>
   )
